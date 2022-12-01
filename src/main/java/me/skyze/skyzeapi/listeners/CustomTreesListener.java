da28d9bfc62e587f25d150791dd1225f7f160419
Michal Hicz <michal07.h@gmail.com>
2022-12-01 12:06:29 +0100

    improved CustomTreesListener

--- src/main/java/me/skyze/skyzeapi/listeners/CustomTreesListener.java
+++ src/main/java/me/skyze/skyzeapi/listeners/CustomTreesListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.units.qual.C;
 
 import java.util.Collection;
 
@@ -20,45 +21,69 @@ public class CustomTreesListener implements Listener {
 
         Action a = e.getAction();
         Player p = e.getPlayer();
-        int activatedEmeraldRequited = 6;
+        //int activatedEmeraldRequited = 6;
         String activatedEmeraldName = ChatColor.DARK_GREEN +""+ChatColor.BOLD + "Aktivovany Emerald";
         ItemStack bonemeal = new ItemStack(Material.BONE_MEAL, 64);
 
         if (a.equals(Action.RIGHT_CLICK_BLOCK) && e.getClickedBlock() != null) {
             Block clickedBlock = e.getClickedBlock();
-            if (p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(activatedEmeraldName) && p.getInventory().getItemInMainHand().getAmount() >= activatedEmeraldRequited) {
+            if (p.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(activatedEmeraldName)) {
 
                 if (clickedBlock.getType().equals(Material.COAL_ORE) || clickedBlock.getType().equals(Material.DEEPSLATE_COAL_ORE)) {
-                    if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
-                        p.getInventory().getItemInMainHand().subtract(activatedEmeraldRequited);
-                        p.getInventory().removeItem(bonemeal);
-                        spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
-                        spawnTree(clickedBlock, Material.COAL_ORE);
-                    } else {p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne "+bonemeal.getAmount()+"x)");}
+                    if (p.getInventory().getItemInMainHand().getAmount() >= 1) {
+                        if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
+                            p.getInventory().getItemInMainHand().subtract(1);
+                            p.getInventory().removeItem(bonemeal);
+                            spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
+                            spawnTree(clickedBlock, Material.COAL_ORE);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne " + bonemeal.getAmount() + "x)");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED + "Potrebujes 1x aktivavany_emerald!");
+                    }
 
                 } else if (clickedBlock.getType().equals(Material.IRON_ORE) || clickedBlock.getType().equals(Material.DEEPSLATE_IRON_ORE)) {
-                    if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
-                        p.getInventory().getItemInMainHand().subtract(activatedEmeraldRequited);
-                        p.getInventory().removeItem(bonemeal);
-                        spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
-                        spawnTree(clickedBlock, Material.IRON_ORE);
-                    } else {p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne "+bonemeal.getAmount()+"x)");}
+                    if (p.getInventory().getItemInMainHand().getAmount() >= 3) {
+                        if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
+                            p.getInventory().getItemInMainHand().subtract(3);
+                            p.getInventory().removeItem(bonemeal);
+                            spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
+                            spawnTree(clickedBlock, Material.IRON_ORE);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne " + bonemeal.getAmount() + "x)");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED+"Potrebujes 3x aktivovany_emerald!");
+                    }
 
                 } else if (clickedBlock.getType().equals(Material.GOLD_ORE) || clickedBlock.getType().equals(Material.DEEPSLATE_GOLD_ORE)) {
-                    if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
-                        p.getInventory().getItemInMainHand().subtract(activatedEmeraldRequited);
-                        p.getInventory().removeItem(bonemeal);
-                        spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
-                        spawnTree(clickedBlock, Material.GOLD_ORE);
-                    } else {p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne "+bonemeal.getAmount()+"x)");}
+                    if (p.getInventory().getItemInMainHand().getAmount() >= 6) {
+                        if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
+                            p.getInventory().getItemInMainHand().subtract(6);
+                            p.getInventory().removeItem(bonemeal);
+                            spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
+                            spawnTree(clickedBlock, Material.GOLD_ORE);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne " + bonemeal.getAmount() + "x)");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED+"Potrebujes 6x aktivovany_emerald!");
+                    }
 
                 } else if (clickedBlock.getType().equals(Material.DIAMOND_ORE) || clickedBlock.getType().equals(Material.DEEPSLATE_DIAMOND_ORE)) {
-                    if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
-                        p.getInventory().getItemInMainHand().subtract(activatedEmeraldRequited);
-                        p.getInventory().removeItem(bonemeal);
-                        spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
-                        spawnTree(clickedBlock, Material.DIAMOND_ORE);
-                    } else {p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne "+bonemeal.getAmount()+"x)");}
+                    if (p.getInventory().getItemInMainHand().getAmount() >= 12) {
+                        if (p.getInventory().contains(Material.BONE_MEAL, 64)) {
+                            p.getInventory().getItemInMainHand().subtract(12);
+                            p.getInventory().removeItem(bonemeal);
+                            spawnParticleForEveryPlayer(clickedBlock.getLocation(), Color.GREEN, Color.GREEN, 0.5F);
+                            spawnTree(clickedBlock, Material.DIAMOND_ORE);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "Nemas dostatok bonemeal! (potrebne " + bonemeal.getAmount() + "x)");
+                        }
+                    } else {
+                        p.sendMessage(ChatColor.RED+"Potrebujes 12x aktivovany_emerald!");
+                    }
 
                 }
             }
--- src/main/java/me/skyze/skyzeapi/listeners/VianoceListener.java
+++ src/main/java/me/skyze/skyzeapi/listeners/VianoceListener.java
@@ -5,9 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
-import org.bukkit.entity.Firework;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Snowball;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
@@ -22,17 +20,19 @@ public class VianoceListener implements Listener {
 
     private Random r = new Random();
 
-    @EventHandler
-    public void onSnowballThrow(ProjectileLaunchEvent e){
-
-        if (e.getEntity() instanceof Snowball s) {
-            if (s.getShooter() instanceof Player p) {
-                if (s.getNearbyEntities(1,1,1) instanceof Player pp) {
-                    pp.setHealth(pp.getHealth()-2);
-                }
-            }
-        }
-    }
+//    @EventHandler
+//    public void onSnowballThrow(ProjectileLaunchEvent e){
+//
+//        if (e.getEntity() instanceof Snowball s) {
+//            if (s.getShooter() instanceof Player p) {
+//                for (Entity ep : s.getNearbyEntities(1,1,1)) {
+//                    if (ep instanceof Mob pp) {
+//                        pp.setHealth(pp.getHealth() - 2);
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     @EventHandler
     public void onSwapHands(PlayerSwapHandItemsEvent e) {
@@ -57,7 +57,7 @@ public class VianoceListener implements Listener {
     public void onPlayerMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         if (p.getInventory().contains(Material.GUNPOWDER)) {
-            if (r.nextInt(1000) < 5) {
+            if (r.nextInt(1000) < 2) {
                 Firework firework = p.getWorld().spawn(p.getLocation(), Firework.class);
                 FireworkMeta data = (FireworkMeta) firework.getFireworkMeta();
                 data.addEffects(FireworkEffect.builder().withColor(Color.RED).withColor(Color.GREEN).with(FireworkEffect.Type.BALL_LARGE).withFlicker().build());
